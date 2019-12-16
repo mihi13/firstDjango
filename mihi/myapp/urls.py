d25503844ebb0ebac0b6b4f7e7b833111d9f954f
mihi13 <jannatfardush13@gmail.com>
2019-12-15 20:42:49 -0800

    updatge

--- mihi/myapp/urls.py
+++ mihi/myapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="Index"),
+    path('',views.index,name="index"),
+    path('about-us/', views.about, name="about"),
+    path('contact-us/', views.contact, name="contact"),
 ]
